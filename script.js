31504f8bbdb34cb5fee1a433aaf7a0582504d3ea
CornIshCow <milton.le@elev.ga.lbs.se>
2026-01-14 14:25:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("loginForm");
+const message = document.getElementById("message");
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    if (email && password) {
+      message.style.color = "green";
+      message.textContent = " Inloggning lyckades! (prototyp)";
+      localStorage.setItem("user", email);
+      window.location.href = "dashboard.html";
+
+    } else {
+      message.style.color = "red";
+      message.textContent = " Fyll i alla fält.";
+    }
+  });
+}
+if (window.location.pathname.includes("dashboard")) {
+  const user = localStorage.getItem("user");
+  if (!user) {
+    window.location.href = "index.hmtl";
+  }
+}
+const logoutBtn = document.getElementById("logout");
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", () => {
+    localStorage.removeItem("user");
+    window.location.href = "index.hmtl";
+  });
+}
